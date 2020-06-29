4fee69d55e131276d988f492b3cf00c15f26c8a0
Matt <matthew.morley.ca@gmail.com>
2020-06-29 09:42:30 -0700

    Update doc comment

--- src/main/java/org/photonvision/lib/PhotonUtils.java
+++ src/main/java/org/photonvision/lib/PhotonUtils.java
@@ -19,8 +19,10 @@ public final class PhotonUtils {
    *
    * <p> Units can be converted using the {@link edu.wpi.first.wpilibj.util.Units} class.
    *
-   * @param cameraHeightMeters The height of the camera off the floor in meters.
-   * @param targetHeightMeters The physical height of the camera off the floor in meters.
+   * @param cameraHeightMeters The physical height of the camera off the floor in meters.
+   * @param targetHeightMeters The physical height of the target off the floor in meters. This should be
+   *                           the height of whatever is being targeted (i.e. if the targeting region is set
+   *                           to top, this should be the height of the top of the target).
    * @param cameraPitchRadians The pitch of the camera from the horizontal plane in radians. Positive values up.
    * @param targetPitchRadians The pitch of the target in the camera's lens in radians. Positive values up.
    * @return The estimated distance to the target in meters.
