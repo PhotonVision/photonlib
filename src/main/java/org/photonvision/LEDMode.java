260f3acb2287eba50c36825c899189735420127a
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-09-02 09:45:07 -0400

    Add LEDMode and lazy initialization for settable NT values

--- src/main/java/org/photonvision/LEDMode.java
+++ src/main/java/org/photonvision/LEDMode.java
@@ -17,25 +17,22 @@
 
 package org.photonvision;
 
-import edu.wpi.first.networktables.NetworkTablesJNI;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
-;
+/**
+ * Contains enum values for the LED Mode.
+ */
+public enum LEDMode {
 
-import java.io.IOException;
+  kDefault(-1), kOn(1), kOff(0), kBlink(2);
 
-class PhotonCameraTest {
-    @Test
-    public void testEmpty() {
-        Assertions.assertDoesNotThrow(() -> {
-            var packet = new Packet(1);
-            var ret = new PhotonPipelineResult();
-            packet.setData(new byte[0]);
-            if(packet.size < 1) {
-                return;
-            }
-            ret.createFromPacket(packet);
-        });
-    }
+  // NT value.
+  final int value;
+
+  /**
+   * Creates an LEDMode enum instance.
+   *
+   * @param value NT value
+   */
+  LEDMode(int value) {
+    this.value = value;
+  }
 }
