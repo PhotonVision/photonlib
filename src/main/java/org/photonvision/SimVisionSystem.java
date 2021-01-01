e81d13d6593191952cb257f2d78c9ac975ca8835
Chris <chrisgerth010592@gmail.com>
2021-01-01 14:39:18 -0600

    Review comment updates

--- src/main/java/org/photonvision/SimVisionSystem.java
+++ src/main/java/org/photonvision/SimVisionSystem.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2020 Photon Vision.
+ * Copyright (C) 2020-2021 Photon Vision.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,55 +17,57 @@
 
 package org.photonvision;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Transform2d;
-import edu.wpi.first.wpiutil.math.Pair;
 
 public class SimVisionSystem {
     SimPhotonCamera cam;
 
-    double camDiagFOV_deg;
-    double camHorizFOV_deg;
-    double camVertFOV_deg;
-    double cameraHeightOffGround_m;
-    double maxLEDRange_m;
-    double camPitch_deg;
-    Pair<Integer, Integer> cameraRes_px;
+    double camDiagFOVDegrees;
+    double camHorizFOVDegrees;
+    double camVertFOVDegrees;
+    double cameraHeightOffGroundMeters;
+    double maxLEDRangeMeters;
+    double camPitchDegrees;
+    int cameraResWidth;
+    int cameraResHeight;
     double minTargetArea;
     Transform2d robotToCamera;
 
-    LinkedList<SimVisionTarget> tgtList;
+    ArrayList<SimVisionTarget> tgtList;
 
     /**
      * Create a simulated vision system involving a camera and coprocessor mounted on a mobile robot running Photonvision, detecting one or more targets scattered around the field.
      * This assumes a fairly simple and distortionless pinhole camera model.
      * @param camName Name of the photonvision camera to create. Align it with the settings you use in the PhotonVision GUI.
-     * @param camDiagFOV_deg Diagonal Field of View of the camera used. Align it with the manufacturer specifications, and/or whatever is configured in the PhotonVision Setting page.
-     * @param camPitch_deg pitch of the camera's view axis back from horizontal. Make this the same as whatever is configured in the PhotonVision Setting page.
+     * @param camDiagFOVDegrees Diagonal Field of View of the camera used. Align it with the manufacturer specifications, and/or whatever is configured in the PhotonVision Setting page.
+     * @param camPitchDegrees pitch of the camera's view axis back from horizontal. Make this the same as whatever is configured in the PhotonVision Setting page.
      * @param robotToCamera Pose Transform to move from the robot's position to the camera's mount position
-     * @param cameraHeightOffGround_m Height of the camera off the ground in meters
-     * @param maxLEDRange_m Maximum distance at which your camera can illuminate the target and make it visible. Set to 9000 or more if your vision system does not rely on LED's.
-     * @param cameraRes_px Pair of integers representing the Width and Height (respectively) of your camera's image sensor in pixels
+     * @param cameraHeightOffGroundMeters Height of the camera off the ground in meters
+     * @param maxLEDRangeMeters Maximum distance at which your camera can illuminate the target and make it visible. Set to 9000 or more if your vision system does not rely on LED's.
+     * @param cameraResWidth Width of your camera's image sensor in pixels
+     * @param cameraResHeight Height of your camera's image sensor in pixels
      * @param minTargetArea Minimum area that that the target should be before it's recognized as a target by the camera. Match this with your contour filtering settings in the PhotonVision GUI.
      */
-    public SimVisionSystem(String camName, double camDiagFOV_deg, double camPitch_deg, Transform2d robotToCamera, double cameraHeightOffGround_m, double maxLEDRange_m, Pair<Integer, Integer> cameraRes_px, double minTargetArea){
-        this.camDiagFOV_deg = camDiagFOV_deg;
-        this.camPitch_deg = camPitch_deg;
+    public SimVisionSystem(String camName, double camDiagFOVDegrees, double camPitchDegrees, Transform2d robotToCamera, double cameraHeightOffGroundMeters, double maxLEDRangeMeters, int cameraResWidth, int cameraResHeight, double minTargetArea){
+        this.camDiagFOVDegrees = camDiagFOVDegrees;
+        this.camPitchDegrees = camPitchDegrees;
         this.robotToCamera  = robotToCamera;
-        this.cameraHeightOffGround_m  = cameraHeightOffGround_m;
-        this.maxLEDRange_m = maxLEDRange_m;
-        this.cameraRes_px = cameraRes_px;
+        this.cameraHeightOffGroundMeters  = cameraHeightOffGroundMeters;
+        this.maxLEDRangeMeters = maxLEDRangeMeters;
+        this.cameraResWidth = cameraResWidth;
+        this.cameraResHeight = cameraResHeight;
         this.minTargetArea = minTargetArea;
 
         //Calculate horizontal/vertical FOV by similar triangles
-        double hypotPixels = Math.hypot(cameraRes_px.getFirst(), cameraRes_px.getSecond());
-        this.camHorizFOV_deg = camDiagFOV_deg * cameraRes_px.getFirst()/hypotPixels;
-        this.camVertFOV_deg  = camDiagFOV_deg * cameraRes_px.getSecond()/hypotPixels;
+        double hypotPixels = Math.hypot(cameraResWidth, cameraResHeight);
+        this.camHorizFOVDegrees = camDiagFOVDegrees * cameraResWidth/hypotPixels;
+        this.camVertFOVDegrees  = camDiagFOVDegrees * cameraResHeight/hypotPixels;
 
         cam = new SimPhotonCamera(camName);
-        tgtList = new LinkedList<SimVisionTarget>();
+        tgtList = new ArrayList<SimVisionTarget>();
     }
 
     /**
@@ -82,37 +84,37 @@ public class SimVisionSystem {
      * Adjust the camera position relative to the robot.
      * Use this if your camera is on a gimbal or turret or some other mobile platform.
      * @param newRobotToCamera New Tranform from the robot to the camera
-     * @param newCamHeight New height of the camera off the floor
+     * @param newCamHeightMeters New height of the camera off the floor
      */
-    public void moveCamera(Transform2d newRobotToCamera, double newCamHeight){
+    public void moveCamera(Transform2d newRobotToCamera, double newCamHeightMeters){
         this.robotToCamera  = newRobotToCamera;
-        this.cameraHeightOffGround_m  = newCamHeight;
+        this.cameraHeightOffGroundMeters  = newCamHeightMeters;
     }
 
     /**
      * Periodic update. Call this once per frame of image data you wish to process and send to NetworkTables
-     * @param robotPose current pose of the robot on the field. Will be used to calcualte which targets are actually in view, where they are at relative to the robot, and relevant PhotonVision parameters.
+     * @param robotPoseMeters current pose of the robot on the field. Will be used to calcualte which targets are actually in view, where they are at relative to the robot, and relevant PhotonVision parameters.
      */
-    public void processFrame(Pose2d robotPose){
+    public void processFrame(Pose2d robotPoseMeters){
 
-        Pose2d cameraPos = robotPose.transformBy(robotToCamera);
+        Pose2d cameraPos = robotPoseMeters.transformBy(robotToCamera);
 
-        LinkedList<PhotonTrackedTarget> visibleTgtList = new LinkedList<PhotonTrackedTarget>();
+        ArrayList<PhotonTrackedTarget> visibleTgtList = new ArrayList<PhotonTrackedTarget>(tgtList.size());
 
         tgtList.forEach((tgt) -> {
             var camToTargetTrans = new Transform2d(cameraPos, tgt.targetPos);
 
-            double dist_along_ground_m = camToTargetTrans.getTranslation().getNorm();
-            double dist_vertical_m = tgt.targetHeight_m - this.cameraHeightOffGround_m;
-            double dist_m = Math.hypot(dist_along_ground_m, dist_vertical_m);
+            double distAlongGroundMeters = camToTargetTrans.getTranslation().getNorm();
+            double distVerticalMeters = tgt.targetHeightAboveGroundMeters - this.cameraHeightOffGroundMeters;
+            double distMeters = Math.hypot(distAlongGroundMeters, distVerticalMeters);
 
-            double area_px = tgt.tgtArea_m2 / getM2PerPx(dist_along_ground_m);
+            double area = tgt.tgtAreaMeters2 / getM2PerPx(distAlongGroundMeters);
 
-            double yaw_deg = PhotonUtils.wrapAngleDeg(camToTargetTrans.getRotation().getDegrees());
-            double pitch_deg = Math.atan2(dist_vertical_m, dist_along_ground_m) - this.camPitch_deg;
+            double yawDegrees = PhotonUtils.wrapAngleDeg(camToTargetTrans.getRotation().getDegrees());
+            double pitchDegrees = Math.atan2(distVerticalMeters, distAlongGroundMeters) - this.camPitchDegrees;
 
-            if(camCanSeeTarget(dist_m, yaw_deg, pitch_deg, area_px)){
-                visibleTgtList.add(new PhotonTrackedTarget(yaw_deg, pitch_deg, area_px, 0.0, camToTargetTrans));
+            if(camCanSeeTarget(distMeters, yawDegrees, pitchDegrees, area)){
+                visibleTgtList.add(new PhotonTrackedTarget(yawDegrees, pitchDegrees, area, 0.0, camToTargetTrans));
             }
         });
 
@@ -121,18 +123,18 @@ public class SimVisionSystem {
     }
 
     double getM2PerPx(double dist){
-        double widthMPerPx  = 2 * dist * Math.tan(this.camHorizFOV_deg/2) / this.cameraRes_px.getFirst();
-        double heightMPerPx = 2 * dist * Math.tan(this.camVertFOV_deg/2)  / this.cameraRes_px.getSecond();
+        double widthMPerPx  = 2 * dist * Math.tan(this.camHorizFOVDegrees/2) / cameraResWidth;
+        double heightMPerPx = 2 * dist * Math.tan(this.camVertFOVDegrees/2)  / cameraResHeight;
         return widthMPerPx * heightMPerPx;
     }
 
 
-    boolean camCanSeeTarget(double dist_m, double yaw, double pitch, double area){
-        boolean inRange = (dist_m < this.maxLEDRange_m);
-        boolean inHorizAngle = Math.abs(yaw) < (this.camHorizFOV_deg/2);
-        boolean inVertAngle = Math.abs(pitch) <  (this.camVertFOV_deg/2);
+    boolean camCanSeeTarget(double distMeters, double yaw, double pitch, double area){
+        boolean inRange = (distMeters < this.maxLEDRangeMeters);
+        boolean inHorizAngle = Math.abs(yaw) < (this.camHorizFOVDegrees/2);
+        boolean inVertAngle = Math.abs(pitch) <  (this.camVertFOVDegrees/2);
         boolean targetBigEnough = area > this.minTargetArea;
-        return (inRange & inHorizAngle & inVertAngle & targetBigEnough);
+        return (inRange && inHorizAngle && inVertAngle && targetBigEnough);
     }
 
 
--- src/main/java/org/photonvision/SimVisionTarget.java
+++ src/main/java/org/photonvision/SimVisionTarget.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2020 Photon Vision.
+ * Copyright (C) 2020-2021 Photon Vision.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,27 +18,29 @@
 package org.photonvision;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpiutil.math.Pair;
 
 public class SimVisionTarget {
     Pose2d targetPos;
-    double targetHeight_m;
-    Pair<Double, Double> targetDims_m;
+    double targetWidthMeters;
+    double targetHeightMeters;
+    double targetHeightAboveGroundMeters;
     double targetInfill_pct;
-    double tgtArea_m2;
+    double tgtAreaMeters2;
 
     /**
-     * Describes a vision target located somewhere on the field that your SimVisionSystem should detect.
+     * Describes a vision target located somewhere on the field that your SimVisionSystem can detect.
      *
      * @param targetPos Pose2d of the target on the field. Define it such that, if you are standing on the middle of the field facing the target, the Y axis points to your left, and the X axis points away from you.
-     * @param targetHeight_m Height of the target above the field plane, in meters.
-     * @param targetDims_m Pair of floating point values describing the width and height of the outter bounding box of the target. Both are in meters.
+     * @param targetHeightAboveGroundMeters Height of the target above the field plane, in meters.
+     * @param targetWidthMeters Width of the outter bounding box of the target in meters.
+     * @param targetHeightMeters Pair Height of the outter bounding box of the target in meters.
      */
-    public SimVisionTarget(Pose2d targetPos, double targetHeight_m, Pair<Double, Double> targetDims_m){
+    public SimVisionTarget(Pose2d targetPos, double targetHeightAboveGroundMeters, double targetWidthMeters, double targetHeightMeters){
         this.targetPos = targetPos;
-        this.targetHeight_m = targetHeight_m;
-        this.targetDims_m = targetDims_m;
-        this.tgtArea_m2 = this.targetDims_m.getFirst() * this.targetDims_m.getSecond();
+        this.targetHeightAboveGroundMeters = targetHeightAboveGroundMeters;
+        this.targetWidthMeters = targetWidthMeters;
+        this.targetHeightMeters = targetHeightMeters;
+        this.tgtAreaMeters2 = targetWidthMeters * targetHeightMeters;
     }
 
 }
