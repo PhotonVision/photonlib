94232574b220c0640034e317239080bd27008ef9
Chris <chrisgerth010592@gmail.com>
2021-01-02 14:40:32 -0600

    WIP C++ skeleton

---
+++ src/main/native/include/photonlib/SimPhotonCamera.h
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2020 Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "photonlib/PhotonCamera.h"
+
+namespace photonlib {
+
+/**
+ * Represents a camera that is connected to PhotonVision.ß
+ */
+class SimPhotonCamera {
+ public:
+  /**
+   * Constructs a Simulated PhotonCamera from a root table.
+   *
+   * @param rootTable The root table that the camera is broadcasting information
+   *                  over.
+   */
+  explicit SimPhotonCamera(std::shared_ptr<nt::NetworkTable> rootTable);
+
+  /**
+   * Constructs a Simulated PhotonCamera from the name of the camera.
+   *
+   * @param cameraName The nickname of the camera (found in the PhotonVision
+   *                   UI).
+   */
+  explicit SimPhotonCamera(const std::string& cameraName);
+
+  /**
+   * Simulate one processed frame of vision data, putting one result to NT.
+   * @param latencyMillis
+   * @param tgtList List of targets detected
+   */
+  void submitProcessedFrame(double latencyMillis, std::initializer_list<PhotonTrackedTarget *> tgtList);
+
+  private:
+  PhotonCamera * cam;
+
+
+};
+
+}  // namespace photonlib
